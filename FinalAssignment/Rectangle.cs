9f87042d5d499555a8a22e8b84e6cfb5a1b5e2d9
Jasman <jasman10mahato@gmail.com>
2020-05-11 21:47:56 +0545

    rect change

--- FinalAssignment/Rectangle.cs
+++ FinalAssignment/Rectangle.cs
@@ -45,7 +45,10 @@ namespace FinalAssignment
             this.texturestyle = texturestyle;
             this.bb = bb;
             this.c1 = c1;
-            
+            this.x = list[0];
+            this.y = list[1];
+            this.size = list[2];
+            this.size1 = list[3];
         }
     }
 }
