834623797af505fba713c8289a329fa83fbbcd9d
Jasman <jasman10mahato@gmail.com>
2020-05-26 15:04:42 +0545

    triangle

--- UnitTestProjectProgram/TriangleTest.cs
+++ UnitTestProjectProgram/TriangleTest.cs
@@ -17,7 +17,7 @@ namespace UnitTestProjectProgram
 
          void TestMethod1()
         {
-           
+            var t = new FinalAssignment.Triangle();
             int xi1 = 100, yi1 = 200, xi2 = 200, yi2 = 200, xii1 = 200, yii1 = 200, xii2 = 200, yii2 = 200, xiii1 = 200, yiii1 = 200, xiii2 = 200, yiii2 = 200;
             t.set(texturestyle, bb, c1, xi1, yi1, xi2, yi2, xii1, yii1, xii2, yii2, xiii1, yiii1, xiii2, yiii2);
             Assert.AreEqual(100, t.xi1);
