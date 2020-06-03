98cc51ed2b2e4ea74e317fe1c9e1d27829333c27
Jasman <jasman10mahato@gmail.com>
2020-06-03 20:21:34 +0545

    shape 2

--- FinalAssignment/ShapeFactory.cs
+++ FinalAssignment/ShapeFactory.cs
@@ -6,4 +6,29 @@ using System.Threading.Tasks;
 
 namespace FinalAssignment
 {
-    
+    class ShapeFactory
+    {
+        /// <summary>
+        /// checking the type of shape
+        /// </summary>
+        /// <param name="shapeType"></param>
+        /// <returns>return shape object </returns>
+        public Shape GetShape(string shapeType)
+        {
+            if (shapeType == "circle")
+            {
+                return new Circle();
+            }
+            else if (shapeType == "rectangle")
+            {
+                return new Rectangle();
+            }
+
+            else if (shapeType == "triangle")
+            {
+                return new Triangle();
+            }
+            
+        }
+    }
+}
